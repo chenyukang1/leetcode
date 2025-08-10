fb8f3801e47a93ed0101333e238635e939abc60b
yukang.chen <yukang.chen@wz-inc.com>
2025-08-10 11:22:20 +0800

    feat(leetcode): 加一

--- 初级算法/数组/两个数组的交集II.go
+++ 初级算法/数组/两个数组的交集II.go
@@ -1,5 +1,7 @@
 package 数组
 
+import "sort"
+
 /**
 给你两个整数数组nums1 和 nums2 ，请你以数组形式返回两数组的交集。返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。可以不考虑输出结果的顺序。
 
@@ -24,6 +26,7 @@ package 数组
 如果nums2的元素存储在磁盘上，内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
 */
 
+// 用两个哈希表
 func intersect(nums1 []int, nums2 []int) []int {
 	arr1 := make([]int, 1001)
 	for _, num := range nums1 {
@@ -37,7 +40,12 @@ func intersect(nums1 []int, nums2 []int) []int {
 
 	var ans []int
 	for i := 0; i < 1001; i++ {
-		m := min2(arr1[i], arr2[i])
+		var m int
+		if arr1[i] <= arr2[i] {
+			m = arr1[i]
+		} else {
+			m = arr2[i]
+		}
 		for m > 0 {
 			ans = append(ans, i)
 			m--
@@ -46,9 +54,22 @@ func intersect(nums1 []int, nums2 []int) []int {
 	return ans
 }
 
-func min2(a int, b int) int {
-	if a <= b {
-		return a
+// 先排序，双指针
+func intersect2(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	var ans []int
+	for i, j := 0, 0; i < len(nums1) && j < len(nums2); {
+		if nums1[i] == nums2[j] {
+			ans = append(ans, nums1[i])
+			i++
+			j++
+		}
+		if nums1[i] < nums2[j] {
+			i++
+		} else {
+			j++
+		}
 	}
-	return b
+	return ans
 }
