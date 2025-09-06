147538ab7be8d7da96ca9b28e554c175b6dd0068
yukang.chen <chen.yukang@qq.com>
2025-09-07 00:33:51 +0800

    feat(data_structure): Add ADT test

--- datasructure/tree/adt.go
+++ datasructure/tree/adt.go
@@ -1,9 +1,12 @@
 package tree
 
+import "fmt"
+
 type Comparable[T any] interface {
 	CompareTo(other T) int
 }
 
+// BinaryNode 二叉查找树 ADT
 type BinaryNode[T Comparable[T]] struct {
 	Left  *BinaryNode[T]
 	Right *BinaryNode[T]
@@ -37,4 +40,63 @@ func Insert[T Comparable[T]](data T, node *BinaryNode[T]) *BinaryNode[T] {
 		// duplicate
 	}
 	return node
+}
+
+func Remove[T Comparable[T]](data T, node *BinaryNode[T]) *BinaryNode[T] {
+	if node == nil {
+		return node // not found
+	}
+	res := data.CompareTo(node.Data)
+	if res < 0 {
+		node.Left = Remove(data, node.Left)
+	} else if res > 0 {
+		node.Right = Remove(data, node.Right)
+	} else if node.Left != nil && node.Right != nil {
+		// 有两个叶子节点的情况：先找到右子树的最小值，即比当前根节点大的最小值
+		// 替换当前根节点的值，递归删除右子树的那个值
+		node.Data = findMin(node.Right).Data
+		node.Right = Remove(node.Data, node.Right)
+	} else {
+		if node.Left == nil {
+			node = node.Right
+		} else {
+			node = node.Left
+		}
+	}
+	return node
+}
+
+func findMin[T Comparable[T]](node *BinaryNode[T]) *BinaryNode[T] {
+	if node == nil {
+		return nil
+	}
+	if node.Left == nil {
+		return node
+	}
+	return findMin(node.Left)
+}
+
+func Print[T Comparable[T]](root *BinaryNode[T]) {
+	if root == nil {
+		return
+	}
+	queue := []BinaryNode[T]{*root}
+	var ans [][]T
+	for len(queue) > 0 {
+		size := len(queue)
+		var temp []T
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if node.Left != nil {
+				queue = append(queue, *node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, *node.Right)
+			}
+			temp = append(temp, node.Data)
+		}
+		ans = append(ans, temp)
+	}
+	fmt.Println(ans)
 }
--- datasructure/tree/adt_test.go
+++ datasructure/tree/adt_test.go
@@ -53,3 +53,71 @@ func TestInsert(t *testing.T) {
 		t.Errorf("expected 3, got %d!", value)
 	}
 }
+
+func TestRemove(t *testing.T) {
+	root := &BinaryNode[IntWrapper]{
+		Data: IntWrapper{Value: 6},
+	}
+	n1 := &BinaryNode[IntWrapper]{
+		Data: IntWrapper{Value: 2},
+	}
+	n2 := &BinaryNode[IntWrapper]{
+		Data: IntWrapper{Value: 8},
+	}
+	n3 := &BinaryNode[IntWrapper]{
+		Data: IntWrapper{Value: 1},
+	}
+	n4 := &BinaryNode[IntWrapper]{
+		Data: IntWrapper{Value: 3},
+	}
+	n5 := &BinaryNode[IntWrapper]{
+		Data: IntWrapper{Value: 4},
+	}
+	root.Left = n1
+	root.Right = n2
+	n1.Left = n3
+	n1.Right = n5
+	n5.Left = n4
+	Remove(IntWrapper{Value: 3}, root)
+	Print(root)
+	if n5.Left != nil {
+		t.Errorf("left expected nil, got %d!", n5.Left.Data)
+	}
+
+	Remove(IntWrapper{Value: 4}, root)
+	Print(root)
+	if n1.Right != nil {
+		t.Errorf("right expected nil, got %d!", n1.Right.Data)
+	}
+}
+
+func TestRemove_2node(t *testing.T) {
+	root := &BinaryNode[IntWrapper]{
+		Data: IntWrapper{Value: 6},
+	}
+	n1 := &BinaryNode[IntWrapper]{
+		Data: IntWrapper{Value: 2},
+	}
+	n2 := &BinaryNode[IntWrapper]{
+		Data: IntWrapper{Value: 8},
+	}
+	n3 := &BinaryNode[IntWrapper]{
+		Data: IntWrapper{Value: 1},
+	}
+	n4 := &BinaryNode[IntWrapper]{
+		Data: IntWrapper{Value: 3},
+	}
+	n5 := &BinaryNode[IntWrapper]{
+		Data: IntWrapper{Value: 4},
+	}
+	root.Left = n1
+	root.Right = n2
+	n1.Left = n3
+	n1.Right = n5
+	n5.Left = n4
+	Remove(IntWrapper{Value: 2}, root)
+	Print(root)
+	if root.Left.Data.Value != 3 {
+		t.Errorf("left expected nil, got %d!", root.Left.Data.Value)
+	}
+}
