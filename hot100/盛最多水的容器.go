fcac556d0f26d3135b2a0a4839d5c0fdfcd87a3c
yukang.chen <yukang.chen@wz-inc.com>
2025-09-20 00:41:19 +0800

    feat(leetcode): 盛水最多的容器

---
+++ hot100/盛最多水的容器.go
@@ -0,0 +1,45 @@
+package hot100
+
+// https://leetcode.cn/problems/container-with-most-water
+//
+// 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+//
+// 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+//
+// 返回容器可以储存的最大水量。
+
+//输入：[1,8,6,2,5,4,8,3,7]
+//输出：49
+//解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+
+func maxArea(height []int) int {
+	l, r := 0, len(height)-1
+	m := 0
+	ans := 0
+	for l < r {
+		n := minInt(height[l], height[r])
+		if n > m {
+			ans = maxInt(ans, (r-l)*n)
+		}
+		if height[l] < height[r] {
+			l++
+		} else {
+			r--
+		}
+	}
+	return ans
+}
+
+func minInt(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func maxInt(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
