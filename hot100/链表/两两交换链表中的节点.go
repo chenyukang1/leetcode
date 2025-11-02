3370449a3a03ecc0436ebe6a707f2d9c4b0539e5
yukang.chen <chen.yukang@qq.com>
2025-11-02 15:34:31 +0800

    feat(leetcode): 两两交换链表中的节点

--- hot100/链表/两两交换链表中的节点.go
+++ hot100/链表/两两交换链表中的节点.go
@@ -3,19 +3,23 @@ package 链表
 // https://leetcode.cn/problems/swap-nodes-in-pairs/?envType=study-plan-v2&envId=top-100-liked
 
 func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
 	dummyHead := &ListNode{0, head}
-	prev, cur := dummyHead, dummyHead.Next
-	for i := 0; cur != nil && cur.Next != nil; i++ {
+	prev, cur, next := dummyHead, dummyHead.Next, dummyHead.Next.Next
+	for i := 0; next != nil; i++ {
 		if i%2 == 0 {
-			next := cur.Next
 			prev.Next = next
 			temp := next.Next
 			next.Next = cur
 			cur.Next = temp
 			prev = next
+			next = temp
 		} else {
 			prev = cur
-			cur = cur.Next
+			cur = next
+			next = next.Next
 		}
 	}
 	return dummyHead.Next
