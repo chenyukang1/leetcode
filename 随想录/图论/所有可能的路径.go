8696dd9853afebc9cf34a6146845d945cb36d349
yukang.chen <chen.yukang@qq.com>
2025-12-07 17:07:49 +0800

    feat(leetcode): 所有可能的路径

---
+++ 随想录/图论/所有可能的路径.go
@@ -0,0 +1,25 @@
+package 图论
+
+// https://leetcode.cn/problems/all-paths-from-source-to-target/description/
+
+func allPathsSourceTarget(graph [][]int) [][]int {
+	var res [][]int
+	var path []int
+	path = append(path, 0)
+	allPathsSourceTargetDsf(0, path, &res, graph)
+	return res
+}
+
+func allPathsSourceTargetDsf(start int, path []int, res *[][]int, graph [][]int) {
+	if start == len(graph)-1 {
+		temp := make([]int, len(path))
+		copy(temp, path)
+		*res = append(*res, temp)
+		return
+	}
+	for j := 0; j < len(graph[start]); j++ {
+		path = append(path, graph[start][j])
+		allPathsSourceTargetDsf(graph[start][j], path, res, graph)
+		path = path[:len(path)-1]
+	}
+}
