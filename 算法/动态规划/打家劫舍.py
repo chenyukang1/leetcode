3064ef56767aea9813ff57fe62a1b37d49eab8c6
yukang.chen <chen.yukang@qq.com>
2026-03-14 00:06:01 +0800

    打家劫舍

--- 算法/动态规划/打家劫舍.py
+++ 算法/动态规划/打家劫舍.py
@@ -15,3 +15,17 @@ class Solution:
             dp[i][1] = dp[i - 1][0] + nums[i]
 
         return max(dp[-1][0], dp[-1][1])
+
+    def rob2(self, nums: List[int]) -> int:
+        # dp[i]表示到i房屋为止，能偷到的最大金额
+        # dp[i] = max(dp[i-2]+nums[i],dp[i-1])
+        if len(nums) == 1:
+            return nums[0]
+        dp = [0] * (len(nums))
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+
+        return dp[len(nums) - 1]
